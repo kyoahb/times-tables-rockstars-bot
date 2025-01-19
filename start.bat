5ad520b0ac4300b20883dd91f194f343a1de02db
kyoahb <keoab@icloud.com>
2025-01-19 13:04:54 +0000

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,28 @@
+REM filepath: /c:/Users/bresk/Desktop/proj/ttrbot2/start.bat
 @echo off
-REM Activate the virtual environment
-call .venv\Scripts\activate
+SET VENV_DIR=.venv
+
+REM Check if the virtual environment directory exists
+IF NOT EXIST %VENV_DIR% (
+    echo "Installing Requirements"
+    REM Create the virtual environment
+    py -m venv %VENV_DIR%
+    
+    REM Activate the virtual environment
+    call %VENV_DIR%\Scripts\activate.bat
+
+    echo "Activated VENV"
+
+    REM Install the requirements line by line
+    for /f "tokens=*" %%i in (requirements.txt) do (
+        echo "Installing %%i"
+        pip install %%i
+    )
+) ELSE (
+    REM Activate the virtual environment
+    call %VENV_DIR%\Scripts\activate.bat
+    echo "Activated VENV"
+)
 
 REM Run the Python script
 python main.py
