1a9febc53b22ea58aa221a1315974b6cf88aceeb
kyoahb <keoab@icloud.com>
2025-01-19 12:49:21 +0000

    forgot a single dot

--- start.bat
+++ start.bat
@@ -1,9 +1,10 @@
 @echo off
 REM Activate the virtual environment
-call venv\Scripts\activate
+call .venv\Scripts\activate
 
 REM Run the Python script
 python main.py
 
 REM Deactivate the virtual environment
 deactivate
+pause
